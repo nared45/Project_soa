ba8cb915031e494971fff3315f6a3c16c385713a
SaThitiphon <78350405+SaThitiphon@users.noreply.github.com>
2023-03-31 23:31:43 +0700

    เพิ่ม path /hello?name=

--- Project_soa/src/main/java/com/example/Project_soa/ProjectSoaApplication.java
+++ Project_soa/src/main/java/com/example/Project_soa/ProjectSoaApplication.java
@@ -3,13 +3,20 @@ package com.example.Project_soa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @ComponentScan("com.example")
+@RestController
 public class ProjectSoaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectSoaApplication.class, args);
 	}
-
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	    return String.format("Hello %s!", name);
+	}
 }
