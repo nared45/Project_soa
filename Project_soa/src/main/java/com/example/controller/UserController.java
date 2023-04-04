93f8649bc29ae9eaad6a3e6eaa8f07135b421a01
Nared Fuengverojsakul <66765930+nared45@users.noreply.github.com>
2023-04-05 03:24:25 +0700

    Update UserController.java

--- Project_soa/src/main/java/com/example/controller/UserController.java
+++ Project_soa/src/main/java/com/example/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.example.service.UserService;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin("http://localhost:8082/")
 public class UserController {
 	@Autowired
 	private UserService userService;
